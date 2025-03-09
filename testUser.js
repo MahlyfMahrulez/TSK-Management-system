e36d1364b6a77808796a7d17c4f2e29e6009a528
MahlyfMahrulez <srisaler101001@gmail.com>
2025-03-10 02:30:44 +0530

    TMS-101: Implement User Authentication API

--- testUser.js
+++ testUser.js
@@ -18,8 +18,8 @@ db.connect((err) => {
 });
 
 // ✅ Create a test user with a hashed password
-const email = 'Sri@example.com';
-const plainPassword = '123';
+const email = 'yap@example.com';
+const plainPassword = '1234';
 
 bcrypt.hash(plainPassword, 10, (err, hash) => {
     if (err) {
